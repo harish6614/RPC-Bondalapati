30f47c4c1ec8c0d1ee13828d3412b8db7ff6db3a
student <s530741@ch112011m.nwmissouri.edu>
2018-02-20 15:31:05 -0600

    Added Model

---
+++ RPC-Bondalapati/Model.swift
@@ -0,0 +1,64 @@
+//
+//  Model.swift
+//  RPC-Bondalapati
+//
+//  Created by Bondalapati,Harish on 2/20/18.
+//  Copyright © 2018 Bondalapati,Harish. All rights reserved.
+//
+
+enum Choice { case None, Rock, Paper, Scissor, Spock, Lizard}
+
+class Model {
+    private var player1Choice:Choice
+    private var player2Choice:Choice
+    
+    var player1:Choice { get { return player1Choice } }
+    var player2:Choice { get { return player1Choice } }
+    
+    private init(){
+        player1Choice = Choice.None
+        player2Choice = Choice.None
+    }
+    
+    func reset() {
+        player1Choice = Choice.None
+        player2Choice = Choice.None
+    }
+    
+    func haveResult() -> Bool {
+        return !(Choice.None == player1Choice || Choice.None == player2Choice)
+    }
+    
+    func choosePlayer1(pick:Choice) {
+        player1Choice = pick
+    }
+    
+    func choosePlayer2(pick:Choice) {
+        player2Choice = pick
+    }
+    
+    func  winner() -> String {
+        if(player1Choice == player2Choice) {
+            return "Its a Tie"
+        }
+        
+        retu
+    }
+    
+    private static func conquers(choice:Choice) -> [Choice]  {
+        switch choice {
+        case .Rock:
+            return [Choice.Lizard, Choice.Scissor]
+        case .Paper:
+            return [Choice.Rock, Choice.Spock]
+        case .Scissor:
+            return [Choice.Lizard, Choice.Paper]
+        case .Lizard:
+            return [Choice.Paper, Choice.Spock]
+        case .Spock:
+            return [Choice.Scissor, Choice.Rock]
+        default:
+            return []
+        }
+    }
+}
